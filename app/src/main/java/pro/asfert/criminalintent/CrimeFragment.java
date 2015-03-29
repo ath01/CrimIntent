11a0d88da69a78d302f640df075a63d90f0fe72f
ath01 <darkwawe00@gmail.com>
2015-03-29 22:53:40 +0500

    CrimInt #1.0

--- app/src/main/java/pro/asfert/criminalintent/CrimeFragment.java
+++ app/src/main/java/pro/asfert/criminalintent/CrimeFragment.java
@@ -7,7 +7,6 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
